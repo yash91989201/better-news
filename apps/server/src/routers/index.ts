d964b3934ca10f8c371226317825c08a38aa6289
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-06-30 22:46:23 +0530

    Initial commit

---
+++ apps/server/src/routers/index.ts
@@ -0,0 +1,16 @@
+import { protectedProcedure, publicProcedure } from "../lib/orpc";
+import { todoRouter } from "./todo";
+
+export const appRouter = {
+  healthCheck: publicProcedure.handler(() => {
+    return "OK";
+  }),
+  privateData: protectedProcedure.handler(({ context }) => {
+    return {
+      message: "This is private",
+      user: context.session?.user,
+    };
+  }),
+  todo: todoRouter,
+};
+export type AppRouter = typeof appRouter;
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart({})],
+});
