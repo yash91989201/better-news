167a36dff2305206dabad53af1e6aa71bf26c726
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-07-01 12:54:20 +0530

    fix: remove todoRouter from index router

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,16 +1,14 @@
 import { protectedProcedure, publicProcedure } from "../lib/orpc";
-import { todoRouter } from "./todo";
 
 export const appRouter = {
-  healthCheck: publicProcedure.handler(() => {
-    return "OK";
-  }),
-  privateData: protectedProcedure.handler(({ context }) => {
-    return {
-      message: "This is private",
-      user: context.session?.user,
-    };
-  }),
-  todo: todoRouter,
+	healthCheck: publicProcedure.handler(() => {
+		return "OK";
+	}),
+	privateData: protectedProcedure.handler(({ context }) => {
+		return {
+			message: "This is private",
+			user: context.session?.user,
+		};
+	}),
 };
 export type AppRouter = typeof appRouter;
